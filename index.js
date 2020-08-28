924ede80e9048c64fa97c12848fdb4cdd9bc2451
Jonathan Bower <bowerpower85@gmail.com>
2020-08-27 21:30:55 -0700

    Added questions to my index.js and edited some of the markdown.js

--- index.js
+++ index.js
@@ -1,18 +1,55 @@
-ines (11 sloc)  234 Bytes
-  
+const inquirer = require('inquirer');
+const axios = require('axios');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 // array of questions for user
 const questions = [
-
+    {
+        type: 'input',
+        name: 'username',
+        message: 'What is your GitHub username?'
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the projects Title?'
+    },
+    {
+        type: 'input',
+        name: 'repo',
+        message: 'What is the name of your Github repo?'
+    },
+    {
+        type: 'input',
+        name: 'desc',
+        message: 'How would you describe the Project?.'
+    },
+    {
+        type: 'input',
+        name: 'install',
+        message: 'What are the steps required to install your project?'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Write instructions for using your project.'
+    },
+    {
+        type: 'input',
+        name: 'contributors',
+        message: 'Enter contributor names if any.'
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Are there any tests to run on the Project?'
+    },
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
 }
 
-// function to initialize program
-function init() {
-
-}
-
 // function call to initialize program
 init();
+// console.log(questions);
